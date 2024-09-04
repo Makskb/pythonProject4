30a8029f596e84eeabfe81f9087457fa57b93f3b
Makskb <makskb@mail.ru>
2024-09-04 22:59:17 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,9 @@
-first = 147
-second = 258
-third = 369
+first = int(input('first '))
+second = int(input('second '))
+third = int(input('third ' ))
 if first == second == third:
     print(3)
-elif first == second != third or first == third != second:
+elif first == second  or first == third or second == third:
     print(2)
 else:
     print(0)
