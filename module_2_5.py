a0ca3213745b1ed7f58994d0a37069a3dd4b0739
Makskb <makskb@mail.ru>
2024-09-16 15:53:28 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,18 @@
+# 2023/10/03 00:00|Домашняя работа по уроку "Функции в Python.Функция с параметром"
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    print(matrix)
+get_matrix(2, 2, 10)
+get_matrix(3, 5, 42)
+get_matrix(4, 2, 13)
+
+#     result1 = get_matrix(2, 2, 10)
+#     result2 = get_matrix(3, 5, 42)
+#     result3 = get_matrix(4, 2, 13)
+#     # print(result1)
+#     # print(result2)
+#     # print(result3)
